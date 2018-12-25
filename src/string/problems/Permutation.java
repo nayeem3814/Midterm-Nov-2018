faf4ae8f3b26ec397f91e5f37371d7e4be74cd1e
nayeem3814 <nayeemsisir156@gmail.com>
2018-12-24 21:33:48 -0500

    Permutation

--- src/string/problems/Permutation.java
+++ src/string/problems/Permutation.java
@@ -12,5 +12,20 @@ public class Permutation {
          * Write Java program to compute all Permutation of a String
          *
          */
+        permutation("ABC");
+    }
+    public static void permutation(String input) {
+        permutation(" ", input);
+
+    }
+    public static void permutation(String perm, String word) {
+        if(word.isEmpty()) {
+            System.out.println(perm + word);
+        } else {
+            for (int i = 0; i<word.length(); i++) {
+                permutation(perm + word.charAt(i), word.substring(0,i) + word.substring(i + 1, word.length()));
+            }
+        }
+
     }
 }
