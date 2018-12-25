20d5c41a0209fcfff926936ec02d7d812623bc14
nayeem3814 <nayeemsisir156@gmail.com>
2018-12-24 19:56:09 -0500

    FindLowestDifference

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,31 +1,33 @@
 package math.problems;
 
 public class FindLowestDifference {
-    public static void main(String[] args) {
-        /*
-         Implement in java.
+
+        /*Implement in java.
          Read this below two array. Find the lowest difference between the two array cell.
-         The lowest difference between cells is 1
-        */
-        int [] array1 = {30,12,5,9,2,20,33,1};
-        int [] array2 = new int[]{18, 25, 41, 47, 17, 36, 14, 19};
+         The lowest difference between cells is 1*/
 
-        int low = array1[0];
-        for(int i=0;i<array1.length;i++){
-            for(int j=0;j<array2.length; j++){
-
-                int diff = Math.abs(array1[i]-array2[j]);
-                if(diff<low)
-                { low = diff;
+    public static int getSmallest(int[] a, int total){
+        int temp;
+        for (int i = 0; i < total; i++)
+        {
+            for (int j = i + 1; j < total; j++)
+            {
+                if (a[i] > a[j])
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
                 }
             }
         }
-        System.out.println(low);
-
-
-
-
-
+        return a[0];
     }
+    public static void main(String args[]){
+        int a[]={30,12,5,9,2,20,33,1};
+        int b[]={18,25,41,47,17,36,14,19};
+        //System.out.println("Smallest: "+getSmallest(a,8));
+        //System.out.println("Smallest: "+getSmallest(b,8));
+        System.out.println("The smallest number from the two array number is: "+Math.min(getSmallest(a,8), getSmallest(b,8)));
 
-}
+    }}
+
